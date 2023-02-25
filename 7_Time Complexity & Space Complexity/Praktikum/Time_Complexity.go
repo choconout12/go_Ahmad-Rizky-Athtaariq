a285b9a928552f1d0ef9d020aadf3628ece815b2
Iky <ahmadxmia1@gmail.com>
2023-02-25 12:08:58 +0700

    fix soal prioritas

---
+++ 7_Time Complexity & Space Complexity/Praktikum/Time_Complexity.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func primeNumber(number int) bool {
+
+	if number < 2 {
+		return false
+	}
+	for i := 2; i*i <= number; i++ {
+		if number%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(primeNumber(1500450271)) // true
+	fmt.Println(primeNumber(13))         // true
+	fmt.Println(primeNumber(17))         // true
+	fmt.Println(primeNumber(20))         // false
+	fmt.Println(primeNumber(35))         // false
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/choconout12/go_Ahmad-Rizky-Athtaariq
+
+go 1.20
